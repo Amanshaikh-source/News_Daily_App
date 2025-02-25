a2a44390de1bd2c52fbfbda797b1078b0bc6072e
Aman Shaikh <shaikhaman3588@gmail.com>
2025-02-25 18:00:55 +0530

    project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.cardview)
+    implementation(libs.androidx.swiperefreshlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/newsdaily/MainActivity.kt
+++ app/src/main/java/com/example/newsdaily/MainActivity.kt
@@ -2,27 +2,32 @@
 
     import androidx.appcompat.app.AppCompatActivity
     import android.os.Bundle
+    import android.os.Handler
+    import android.os.Looper
     import android.util.Log
+    import android.widget.TextView
     import androidx.appcompat.widget.Toolbar
     import androidx.recyclerview.widget.LinearLayoutManager
     import androidx.recyclerview.widget.RecyclerView
+    import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
     import com.example.newsdaily.ui.theme.ApiClient
     import com.example.newsdaily.ui.theme.ApiService
     import com.example.newsdaily.ui.theme.New
-    import com.example.newsdaily.ui.theme.NewsResponse
     import com.example.newsdaily.ui.theme.RecyclerAdapter
     import kotlinx.coroutines.CoroutineScope
     import kotlinx.coroutines.Dispatchers
     import kotlinx.coroutines.launch
     import kotlinx.coroutines.withContext
-    import retrofit2.Call
-    import retrofit2.Response
+    import java.text.SimpleDateFormat
+    import java.util.Date
 
 
     class MainActivity : AppCompatActivity() {
 
         private lateinit var adapter: RecyclerAdapter
         private lateinit var recyclerView: RecyclerView
+        private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+        private lateinit var tvLastUpdated : TextView
         private val newsList = mutableListOf<New>()
 
 
@@ -41,34 +46,63 @@
             adapter = RecyclerAdapter(newsList)
             recyclerView.adapter = adapter
 
+            tvLastUpdated = findViewById(R.id.tvLastUpdated)
+
+
             fetchnews()
+
+            swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
+            swipeRefreshLayout.setOnRefreshListener {
+                fetchnews()
+            }
+
         }
         override fun onResume() {
             super.onResume()
             fetchnews()
         }
 
-             private fun fetchnews() {
-                val apiService = ApiClient.retrofit.create(ApiService::class.java)
+        private fun updateTimeStamp(){
+
+            val sdf = SimpleDateFormat("hh:mm a, MMM dd") // Example: 03:45 PM, Feb 25
+            val currentTime = sdf.format(Date())
+            tvLastUpdated.text = "Last Updated : $currentTime"
+            tvLastUpdated.alpha = 1f
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                tvLastUpdated.animate()
+                    .alpha(0f)
+                    .setDuration(1000)
+                    .start()
+            },5000)
+        }
+        private fun fetchnews() {
+            val apiService = ApiClient.retrofit.create(ApiService::class.java)
 
                 CoroutineScope(Dispatchers.IO).launch { // Run in background thread
                     try {
                         val response = apiService.getTopHeadlines("7ded0c30f3cffbccdd0cfa3f501e5c9a")
 
-                        withContext(Dispatchers.Main) { // Switch to main thread for UI updates
+                        withContext(Dispatchers.Main) {
+                            swipeRefreshLayout.isRefreshing = false
                             if (response.isSuccessful) {
                                 val newsResponse = response.body()
                                 newsResponse?.let {
                                     newsList.clear()
                                     newsList.addAll(it.articles)
                                     adapter.updateData(newsList)
+                                    updateTimeStamp()
                                 }
                             } else {
                                 Log.e("API_ERROR", "Error Code: ${response.code()}, Message: ${response.errorBody()?.string()}")
                             }
                         }
                     } catch (e: Exception) {
-                        Log.e("NETWORK_ERROR", "Exception: ${e.message}")
+                        withContext(Dispatchers.Main){
+                            swipeRefreshLayout.isRefreshing = false
+                            Log.e("NETWORK_ERROR", "Exception: ${e.message}")
+                        }
+
                     }
                 }
 
