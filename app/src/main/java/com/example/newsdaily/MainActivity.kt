556542834ac6ae24abab7bd4428359afaf3e4139
Aman Shaikh <shaikhaman3588@gmail.com>
2025-02-25 18:12:24 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/newsdaily/MainActivity.kt
    #	app/src/main/java/com/example/newsdaily/ui/theme/ApiClient.kt
    #	app/src/main/java/com/example/newsdaily/ui/theme/New.kt
    #	app/src/main/java/com/example/newsdaily/ui/theme/RecyclerAdapter.kt
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/item_layout.xml

--- app/src/main/java/com/example/newsdaily/MainActivity.kt
+++ app/src/main/java/com/example/newsdaily/MainActivity.kt
@@ -1,5 +1,6 @@
     package com.example.newsdaily
 
+    import android.annotation.SuppressLint
     import androidx.appcompat.app.AppCompatActivity
     import android.os.Bundle
     import android.os.Handler
@@ -31,6 +32,7 @@
         private val newsList = mutableListOf<New>()
 
 
+        @SuppressLint("MissingInflatedId")
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
